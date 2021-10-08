cb24de78f651c7bdb5c702b7d20f3a30cc1b8a09
Kevin Tan Yulo <yulokevin@gmail.com>
2021-10-08 14:20:12 +0900

    concat arrays

---
+++ concat-arrays.js
@@ -0,0 +1,17 @@
+function concat (first, second) {
+  let carray = [];
+  for (let score of first) {
+    carray.push(score);
+  }
+  for (let score of second){
+    carray.push(score);
+  }
+
+  return carray;
+}
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
+
